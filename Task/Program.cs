c422e5070f5ebac7986a7bbf5514499f3734ffdb
XDbitionist <eugene.sakharov@gmail.com>
2022-11-30 17:18:14 +0800

    ver 1

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string[] array1 = new string[5] {"Kazan", "USA", "Russia", "123", "2"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+SecondArray(array1, array2);
+PrintArray(array2);
